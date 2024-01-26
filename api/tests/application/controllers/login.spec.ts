6ed81dfa6b650f59b54f4daa8715b29ebb342184
renan04lima <renan04lima@outlook.com>
2024-01-25 22:05:28 -0300

    test: ensure LoginController works correctly

--- api/tests/application/controllers/login.spec.ts
+++ api/tests/application/controllers/login.spec.ts
@@ -1,58 +1,74 @@
-import { AuthenticationError } from "@/domain/entities/errors/authentication"
-import { ServerError, UnauthorizedError } from "@/application/errors/http"
-import { LoginController } from "@/application/controllers/login"
-
-jest.mock('@/application/validation/composite')
+import { ServerError, UnauthorizedError } from '@/application/errors/http'
+import { LoginController } from '@/application/controllers/login'
 
 describe('LoginController', () => {
-    let sut: LoginController
-    let facebookAuth: jest.Mock
+  let sut: LoginController
+  let auth: jest.Mock
 
-    beforeEach(() => {
-        facebookAuth = jest.fn()
-        sut = new LoginController(facebookAuth)
-        facebookAuth.mockResolvedValue({ accessToken: 'token_value' })
+  beforeEach(() => {
+    auth = jest.fn()
+    sut = new LoginController(auth)
+
+    auth.mockResolvedValue({
+      user: { id: 'user_id', email: 'user_email' },
+      token: 'token_value',
+    })
+  })
+
+  it('should call Authentication with correct params', async () => {
+    await sut.handle({ email: 'any_email@email.com', password: 'any_password' })
+
+    expect(auth).toHaveBeenCalledWith({
+      email: 'any_email@email.com',
+      password: 'any_password',
+    })
+    expect(auth).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 401 if invalid credentials are provided', async () => {
+    auth.mockResolvedValueOnce(undefined)
+
+    const httpResponse = await sut.handle({
+      email: 'any_email@email.com',
+      password: 'any_password',
     })
 
-  
-    it('should call FacebookAuthentication with correct params', async () => {
-        await sut.handle({ token: 'any_token' })
+    expect(httpResponse).toEqual({
+      statusCode: 401,
+      data: new UnauthorizedError(),
+    })
+  })
 
-        expect(facebookAuth).toHaveBeenCalledWith({ token: 'any_token' })
-        expect(facebookAuth).toHaveBeenCalledTimes(1)
+  it('should return 200 if authentication succeeds', async () => {
+    const httpResponse = await sut.handle({
+      email: 'any_email@email.com',
+      password: 'any_password',
     })
 
-    it('should return 401 if invalid credentials are provided', async () => {
-        facebookAuth.mockResolvedValueOnce(undefined)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: {
+        token: 'token_value',
+        user: {
+          email: 'user_email',
+          id: 'user_id',
+        },
+      },
+    })
+  })
 
-        const httpResponse = await sut.handle({ token: 'any_token' })
+  it('should return 500 if authentication throws', async () => {
+    const error = new Error('infra_error')
+    auth.mockRejectedValueOnce(error)
 
-        expect(httpResponse).toEqual({
-            statusCode: 401,
-            data: new UnauthorizedError()
-        })
+    const httpResponse = await sut.handle({
+      email: 'any_email@email.com',
+      password: 'any_password',
     })
 
-    it('should return 200 if authentication succeeds', async () => {
-        const httpResponse = await sut.handle({ token: 'any_token' })
-
-        expect(httpResponse).toEqual({
-            statusCode: 200,
-            data: {
-                accessToken: 'token_value'
-            }
-        })
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: new ServerError(error),
     })
-
-    it('should return 500 if authentication throws', async () => {
-        const error = new Error('infra_error')
-        facebookAuth.mockRejectedValueOnce(error)
-
-        const httpResponse = await sut.handle({ token: 'any_token' })
-
-        expect(httpResponse).toEqual({
-            statusCode: 500,
-            data: new ServerError(error)
-        })
-    })
+  })
 })
