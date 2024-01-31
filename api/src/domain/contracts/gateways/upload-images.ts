ab87ba8687923a9e6b94121e94849fe429b7732e
renan04lima <renan04lima@outlook.com>
2024-01-31 10:24:18 -0300

    feat: ensure CreateRealEstate call UploadImages with correct params

---
+++ api/src/domain/contracts/gateways/upload-images.ts
@@ -0,0 +1,3 @@
+export interface UploadImages {
+  upload: (files: { buffer: Buffer; mimetype: string }[]) => Promise<string>
+}
--- api/src/domain/use-cases/create-real-estate.ts
+++ api/src/domain/use-cases/create-real-estate.ts
@@ -1,15 +1,22 @@
-import { CreateRealEstateRepository } from '../contracts/repos/real-estate-repo'
+import { UploadImages } from '@/domain/contracts/gateways/upload-images'
+import { CreateRealEstateRepository } from '@/domain/contracts/repos/real-estate-repo'
 
-type Params = CreateRealEstateRepository.Params
+type Params = CreateRealEstateRepository.Params & {
+  files: { buffer: Buffer; mimetype: string }[]
+}
 type Result = CreateRealEstateRepository.Result
 
 export type CreateRealEstate = (input: Params) => Promise<Result>
 
 export type Setup = (
   realEstateRepo: CreateRealEstateRepository,
+  uploadImages: UploadImages,
 ) => CreateRealEstate
 
 export const setupCreateRealEstate: Setup =
-  (realEstateRepo) => async (data) => {
+  (realEstateRepo, uploadImages) =>
+  async ({ files, ...data }) => {
+    await uploadImages.upload(files)
+
     return await realEstateRepo.create(data)
   }
